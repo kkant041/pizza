621a48c58583f1691d537a3ec93bcbd01d13e953
kkanr041 <kkant041@gmail.com>
2020-04-02 21:30:32 +0530

    kkant041

--- pizza/settings.py
+++ pizza/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'home.apps.HomeConfig',
     'accounts.apps.AccountsConfig',
     'orders.apps.OrdersConfig',
     'django.contrib.admin',
@@ -120,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
